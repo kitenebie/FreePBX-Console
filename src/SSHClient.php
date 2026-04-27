923dde3577f6d99acc9c268030857914561a95cf
Codego <101083976+kitenebie@users.noreply.github.com>
2026-04-27 16:09:22 +0800

    Add DTLS configuration and database insertion for FreePBX extensions

--- src/SSHClient.php
+++ src/SSHClient.php
@@ -133,6 +133,7 @@ class SSHClient
         $queries = [
             // 1. Insert user
             "INSERT INTO users (extension, name, voicemail, noanswer, recording, outboundcid, sipname, noanswer_cid, busy_cid, chanunavail_cid, noanswer_dest, busy_dest, chanunavail_dest, mohclass) VALUES ('{$ext}', '{$ext}', 'default', '', '', '{$extName}', '', '', '', '', '', '', '', 'default')",
+
             // 2. Insert device
             "INSERT INTO devices (id, tech, dial, devicetype, user, description) VALUES ('{$ext}', 'pjsip', 'PJSIP/{$ext}', 'fixed', '{$ext}', '{$ext}')",
 
@@ -164,49 +165,54 @@ class SSHClient
         ('{$ext}', 'force_rport',                'yes',                25),
         ('{$ext}', 'rewrite_contact',            'yes',                24),
         ('{$ext}', 'rtp_symmetric',              'yes',                23),
-        ('{$ext}', 'rtp_timeout',      '0', 42),
-        ('{$ext}', 'rtp_timeout_hold', '0', 43),
-        ('{$ext}', 'timers',           'no', 32),
-        ('{$ext}', 'timers_min_se',    '0', 33),
+        ('{$ext}', 'rtp_timeout',                '0',                  42),
+        ('{$ext}', 'rtp_timeout_hold',           '0',                  43),
+        ('{$ext}', 'timers',                     'no',                 32),
+        ('{$ext}', 'timers_min_se',              '0',                  33),
         ('{$ext}', 'refer_blind_progress',       'yes',                37),
-        ('{$ext}', 'send_connected_line',        'yes',                 6),
-        ('{$ext}', 'sendrpid',                   'pai',                 8),
-        ('{$ext}', 'trustrpid',                  'yes',                 5),
-        ('{$ext}', 'aggregate_mwi',              'yes',                27),
-        ('{$ext}', 'mwi_subscription',           'auto',               26),
-        ('{$ext}', 'qualifyfreq',                '60',                  9),
-        ('{$ext}', 'device_state_busy_at',       '0',                  38),
-        ('{$ext}', 'remove_existing',            'yes',                21),
-        ('{$ext}', 'outbound_auth',              'yes',                45),
-        ('{$ext}', 'outbound_proxy',             '',                   44),
-        ('{$ext}', 'transport',                  '',                   10),
-        ('{$ext}', 'namedcallgroup',             '',                   14),
-        ('{$ext}', 'namedpickupgroup',           '',                   15),
-        ('{$ext}', 'match',                      '',                   39),
-        ('{$ext}', 'minimum_expiration',         '60',                 41),
-        ('{$ext}', 'maximum_expiration',         '7200',               40),
-        ('{$ext}', 'defaultuser',                '',                    4),
-        ('{$ext}', 'media_address',              '',                   35),
-        ('{$ext}', 'message_context',            '',                   46),
-        ('{$ext}', 'user_eq_phone',              'no',                  7)",
+        ('{$ext}', 'send_connected_line',        'yes',                  6),
+        ('{$ext}', 'sendrpid',                   'pai',                  8),
+        ('{$ext}', 'trustrpid',                  'yes',                  5),
+        ('{$ext}', 'aggregate_mwi',              'yes',                 27),
+        ('{$ext}', 'mwi_subscription',           'auto',                26),
+        ('{$ext}', 'qualifyfreq',                '60',                   9),
+        ('{$ext}', 'device_state_busy_at',       '0',                   38),
+        ('{$ext}', 'remove_existing',            'yes',                 21),
+        ('{$ext}', 'outbound_auth',              'yes',                 45),
+        ('{$ext}', 'outbound_proxy',             '',                    44),
+        ('{$ext}', 'transport',                  '',                    10),
+        ('{$ext}', 'namedcallgroup',             '',                    14),
+        ('{$ext}', 'namedpickupgroup',           '',                    15),
+        ('{$ext}', 'match',                      '',                    39),
+        ('{$ext}', 'minimum_expiration',         '60',                  41),
+        ('{$ext}', 'maximum_expiration',         '7200',                40),
+        ('{$ext}', 'defaultuser',                '',                     4),
+        ('{$ext}', 'media_address',              '',                    35),
+        ('{$ext}', 'message_context',            '',                    46),
+        ('{$ext}', 'dtlsenable',                 'yes',                  0),
+        ('{$ext}', 'user_eq_phone',              'no',                   7)",
 
             // 4. Insert pjsip endpoint
             "INSERT INTO pjsip (id, keyword, data, flags) VALUES
-        ('{$ext}',       'type',         'endpoint', 0),
-        ('{$ext}',       'aors',         '{$ext}',   0),
+        ('{$ext}',       'type',         'endpoint',    0),
+        ('{$ext}',       'aors',         '{$ext}',      0),
         ('{$ext}',       'auth',         '{$ext}-auth', 0)",
 
             // 5. Insert pjsip auth
             "INSERT INTO pjsip (id, keyword, data, flags) VALUES
-        ('{$ext}-auth',  'type',         'auth',     0),
-        ('{$ext}-auth',  'auth_type',    'userpass', 0),
-        ('{$ext}-auth',  'username',     '{$ext}',   0),
+        ('{$ext}-auth',  'type',         'auth',        0),
+        ('{$ext}-auth',  'auth_type',    'userpass',    0),
+        ('{$ext}-auth',  'username',     '{$ext}',      0),
         ('{$ext}-auth',  'password',     '{$password}', 0)",
 
             // 6. Insert pjsip aor
             "INSERT INTO pjsip (id, keyword, data, flags) VALUES
-        ('{$ext}-aor',   'type',         'aor',      0),
-        ('{$ext}-aor',   'max_contacts', '1',        0)",
+        ('{$ext}-aor',   'type',         'aor',         0),
+        ('{$ext}-aor',   'max_contacts', '1',           0)",
+
+            // 7. Insert certman mapping para sa DTLS
+            "INSERT INTO certman_mapping (id, cid, verify, setup, rekey, auto_generate_cert)
+        VALUES ('{$ext}', 2, 'fingerprint', 'actpass', 0, 0)",
         ];
 
         // Run each query separately
@@ -220,7 +226,17 @@ class SSHClient
             }
         }
 
-        // Reload FreePBX
+        // 8. I-append ang DTLS cert files sa pjsip.endpoint_custom_post.conf
+        $dtlsConfig  = "\n[{$ext}]\n";
+        $dtlsConfig .= "dtls_cert_file=/etc/asterisk/keys/default.crt\n";
+        $dtlsConfig .= "dtls_private_key=/etc/asterisk/keys/default.key\n";
+        $appendCmd   = "echo '{$dtlsConfig}' >> /etc/asterisk/pjsip.endpoint_custom_post.conf 2>&1";
+        $appendOutput = $this->ssh->exec($appendCmd);
+        if (!empty(trim($appendOutput))) {
+            $sqlOutputs[] = "DTLS Config: " . trim($appendOutput);
+        }
+
+        // 9. Reload FreePBX
         $reloadOutput = $this->ssh->exec('fwconsole reload 2>&1');
 
         return [
